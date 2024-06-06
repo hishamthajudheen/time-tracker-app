1ec27273661bf565b4f60c29c04e8873a4c48bb4
Hisham Thajudheen <hishamthajudheen@gmail.com>
2024-06-07 02:03:23 +0530

    Seconf Commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../firebase/config";
+import { useNavigate } from "react-router-dom";
+
+//Authentication Instance
+const auth = getAuth(app);
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginLoading, setloginLoading] = useState(false);
+  const [registerLoading, setregisterLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+//Navigate
+  const navigate = useNavigate();
+
+//Register
+const signUp = async() => {
+  setregisterLoading(true);
+  setError(null);
+  try{
+    //Request to firebase
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert('Signup Successful!')
+    navigate('/reports')
+  }catch(error){
+    setError(error.message);
+  }
+};
+
+const login = async() => {
+  setloginLoading(true);
+  setError(null);
+  try{
+    //Request to firebase
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    console.log(user);
+    alert('Logged in Successfully!')
+    navigate('/reports')
+  }catch(error){
+    setError(error.message);
+  }
+};
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -26,6 +69,7 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
+                  onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
@@ -45,6 +89,7 @@ const Login = () => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
@@ -53,12 +98,14 @@ const Login = () => {
             <div>
               {/* Error here */}
               <button
+              onClick={signUp}
                 type="button"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Register
               </button>
               <button
+              onClick={login}
                 type="button"
                 className="w-full flex mt-3 justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-pink-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
