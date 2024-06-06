a9cf759e3274f0b8eb3bda0f103bf6a6fc83250d
Hisham Thajudheen <hishamthajudheen@gmail.com>
2024-06-07 01:26:04 +0530

    Initial Commit

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnJjDrug5SrKZEMtk4_uYasQBm-3CgF0U",
+  authDomain: "time-tracker-app-f2a0e.firebaseapp.com",
+  projectId: "time-tracker-app-f2a0e",
+  storageBucket: "time-tracker-app-f2a0e.appspot.com",
+  messagingSenderId: "876603894630",
+  appId: "1:876603894630:web:33ca355d974fce0546b6a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
